6b48eee6fa49d366f573a2b27e8ebe7f38382107
Salman Tyagi <salmantyagi15@gmail.com>
2024-08-10 20:12:33 +0530

    using generics contstraints as arguments

--- features/generics.ts
+++ features/generics.ts
@@ -37,3 +37,48 @@ class HoldAnything<T> {
 
 new HoldAnything<string>(['d', 'e', 'f']);
 new HoldAnything<number>([4, 5, 6]);
+
+// Generics with functions
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(['alpha', 'beta', 'gamma']);
+printAnything<number>([8, 3, 5]);
+
+// Generic constraints
+class Car {
+  print(): void {}
+}
+
+class House {
+  print(): void {}
+}
+
+interface Printable {
+  print(): void;
+}
+
+function classOfHouseOrCar<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i].print());
+  }
+}
+
+// classOfHouseOrCar([1, 2, 4]);
+classOfHouseOrCar<House>([new House(), new House()]);
+classOfHouseOrCar<Car>([new Car(), new Car()]);
